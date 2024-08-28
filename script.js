07e2c43854f42f46b3dab4a8613f1a930d4028c3
Malka arain <malkaarain92@gmail.com>
2024-08-29 01:49:29 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+// Question no 1
+
+// var result = 10
+
+// document.write("Result"+ "<br>" + "The value of a is:" + result + "<br>" + "<br>")
+
+// var result = 10
+// document.write("The value of ++a is:" + ++result + "<br>" + "Now the value of a is:" + result + "<br>" + "<br>")
+
+// var result = 10
+// document.write("The value of ++a is:" + ++result + "<br>" + "Now the value of a is:" + ++result + "<br>" + "<br>")
+
+// var result = 10
+// document.write("The value of --a is:" + ++result + "<br>" + "Now the value of a is:" + result + "<br>" + "<br>")
+
+// var result = 10
+// document.write("The value of a-- is:" + ++result + "<br>" + "Now the value of a is:" + --result + "<br>" + "<br>")
+
+
+
+// Question no 2
+
+// var a = 2 
+// var b = 1
+// var result = --a - --b + ++b + b--;
+
+// document.write("a is &nbsp;" + a + "<br>")
+// document.write("b is &nbsp;" + b  + "<br>")
+// document.write("result is &nbsp;" + result  + "<br>")
+
+
+
+
+// Question no 3
+
+// var an = prompt("Please enter your number")
+// alert("hello"+" "+an)
+
+
+
+// Question no 5
+
+// var an = prompt("Please enter your number")
+
+// if(an){
+//     parseInt(an)
+// }else{
+//     andocument.write(number + " x 1 = " + (number * 1) + "<br>");
+//     document.write(number + " x 2 = " + (number * 2) + "<br>");
+//     document.write(number + " x 3 = " + (number * 3) + "<br>");
+//     document.write(number + " x 4 = " + (number * 4) + "<br>");
+//     document.write(number + " x 5 = " + (number * 5) + "<br>");
+//     document.write(number + " x 6 = " + (number * 6) + "<br>");
+//     document.write(number + " x 7 = " + (number * 7) + "<br>");
+//     document.write(number + " x 8 = " + (number * 8) + "<br>");
+//     document.write(number + " x 9 = " + (number * 9) + "<br>");
+//     document.write(number + " x 10 = " + (number * 10) + "<br>");
+
+
+// Question no 6
+
+// var sub_1 = prompt("Write your subject 1");
+// var sub_2 = prompt("Write your subject 2");
+// var sub_3 = prompt("Write your subject 3");
+
+// var total_mark = 100
+
+// var obt_1 = prompt("Write your marks obtained in subject 1");
+// var obt_2 = prompt("Write your marks obtained in subject 2");
+
+// document.write(${sub_1} &nbsp ${total_mark} ${parseInt(obt_1)} ${parseInt(obt_1) / total_mark * 100} <br>);
+// document.write(${sub_2} ${total_mark} ${parseInt(obt_2)} ${parseInt(obt_2) / total_mark * 100} <br>);
+// document.write(${sub_3} ${total_mark} ${parseInt(obt_2)} ${parseInt(obt_2) / total_mark * 100} <br>);
+
+// var total_obt = parseInt(obt_1) + parseInt(obt_2) * 2
+// var total_per = total_obt / 300 * 100
+
+// document.write(&nbsp &nbsp ${total_mark * 3} ${parseInt(obt_2) * 2 + parseInt(obt_1)} ${total_per} <br>);
+
+
+
+
